7af6b66603f6e7da77792c15dcb158324889a556
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2026-04-07 17:26:12 -0700

    Improve copy on the site

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { BContainer } from 'bootstrap-vue-next'
+import { BContainer } from "bootstrap-vue-next";
 </script>
 
 <template>
@@ -7,8 +7,15 @@ import { BContainer } from 'bootstrap-vue-next'
     <header class="mb-4 mb-md-5 page-hero">
       <h1 class="page-title">Pokopia Housing Solver</h1>
       <p>
-        Optimize your Pokopia roommate assignments by matching likeminded Pokemon together and
+        Optimize your Pokopia roommate assignments by matching likeminded Pokémon together and
         finding the items that fulfill the most favorites in a household.
+      </p>
+      <p>
+        I had found several tools for finding highly-ranked roommates for an individual Pokémon, but
+        nothing that would optimize arrangements of an entire set of Pokémon into available housing.
+        This approaches uses agglomerative network clustering to cheaply lump together Pokémon with
+        multiple overlapping favorties, while keeping Pokémon with diverging habitat preferences
+        (e.g. <strong>Bright</strong> vs. <strong>Dark</strong>) under different roofs.
       </p>
       <p>
         Made for fun by <a href="https://matthewlincoln.net">Matt Lincoln</a>, drawing on crucial
