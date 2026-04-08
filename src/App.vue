8c46fcd38a8c02404078aaf30eb2d0ac92543894
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2026-04-07 18:28:06 -0700

    typo

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import { BContainer } from 'bootstrap-vue-next'
       <p>
         I had found several tools for finding highly-ranked roommates for an individual Pokémon, but
         nothing that would optimize arrangements of an entire set of Pokémon into available housing.
-        This approaches uses agglomerative clustering to cheaply lump together Pokémon with multiple
+        This approach uses agglomerative clustering to cheaply lump together Pokémon with multiple
         overlapping favorties, while keeping Pokémon with diverging habitat preferences (e.g.
         <strong>Bright</strong> vs. <strong>Dark</strong>) under different roofs.
       </p>
