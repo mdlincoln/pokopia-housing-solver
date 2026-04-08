72b73b1fdefdae37b0bf1db4d4e67453e6de6c40
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2026-04-07 22:34:56 -0700

    add usage instrumentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createBootstrap } from 'bootstrap-vue-next'
 import { createPinia } from 'pinia'
+import posthog from 'posthog-js'
 import { createApp } from 'vue'
 
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
@@ -9,10 +10,23 @@ import './styles/tropical-theme.css'
 import App from './App.vue'
 import router from './router'
 
+posthog.init(
+  import.meta.env.VITE_POSTHOG_TOKEN || 'phc_tOVguqjWzuJaJmpxvho8ifms2nxCnypWeJD5Dd9bSoW',
+  {
+    api_host: 'https://b.matthewlincoln.net',
+    ui_host: 'https://us.posthog.com',
+    defaults: '2026-01-30',
+  },
+)
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(createBootstrap())
+
+app.config.errorHandler = (err, _instance, _info) => {
+  posthog.captureException(err)
+}
 
 app.mount('#app')
